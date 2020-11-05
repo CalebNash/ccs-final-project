e68856e041df064af556839cf67c52545f31f209
Caleb Nash <calebnash@Calebs-MacBook-Pro.local>
2020-11-05 10:13:41 -0500

    map displays all pins

--- frontend/static/src/components/Map.js
+++ frontend/static/src/components/Map.js
@@ -6,40 +6,55 @@ class Map extends React.Component {
     super(props)
     this.state = {
       locations: [],
-      loadMap: 'john',
+      map: {},
+      maps: {},
     }
     this.renderMarkers = this.renderMarkers.bind(this);
     this.addMarkers = this.addMarkers.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.addToState = this.addToState.bind(this);
   }
 
-  async componentDidMount(){
-    const response = await fetch('api/v1/locations/');
-    const data = await response.json();
-    await this.setState({locations:data});
-    //this.setState({loadMap: true});
-  }
 
   handleClick() {
     this.setState({loadMap: 'jill'});
   }
 
-  addMarkers(map, maps){
-    console.log('happy', this.state.locations);
-    // for(var i = 0; i < locations.length; i++){
-    //   console.log('hello');
-    //   this.renderMarkers(map, maps, locations[i]);
-    // }
+
+  async addToState(map, maps){
+    const response = await fetch('api/v1/locations/');
+    const data = await response.json();
+    await this.setState({locations:data});
+    await this.setState({map: map});
+    await this.setState({maps: maps});
+    this.addMarkers()
+
   }
 
 
-  renderMarkers(map, maps, locations){
-    console.log('hello', locations);
+  addMarkers(){
+    const locations = this.state.locations;
+    // console.log('locations: ', this.state.locations);
+    // console.log('maps: ',this.state.maps);
+    // console.log('map: ',this.state.map);
+    for(var i = 0; i < locations.length; i++){
+      console.log('hello');
+      this.renderMarkers(locations[i]);
+    }
+  }
+
+
+  renderMarkers(location){
+    console.log('hello', location);
+    const map = this.state.map;
+    const maps = this.state.maps;
+    const intLat = parseFloat(location.lat);
+    const intLng = parseFloat(location.lng);
      var infoWindow = new maps.InfoWindow({
-       content: `<h5>Desiard Street Shelter</h5><p>hours: Mon - Sun 6am - 6pm</p> <a href="https://www.google.com/maps/place/807 Desiard St, Monroe, LA 71201">807 Desiard St, Monroe, LA 71201</a>`
+       content: `<h5>${location.name}</h5><p>hours: ${location.hours}</p> <a href="https://www.google.com/maps/place/${location.address}">${location.address}</a>`
      });
      let marker = new maps.Marker({
-     position: { lat: 32.506010, lng: -92.111090 },
+     position: { lat: intLat, lng: intLng },
      map,
      title: 'Desiard Street Shelter',
      });
@@ -54,8 +69,8 @@ class Map extends React.Component {
   render() {
     // let googleMap;
     // const loadMap = this.state.loadmap;
-    console.log(this.state.locations);
-    console.log(this.state.loadMap);
+    console.log('maps: ',this.state.maps);
+    console.log('map: ',this.state.map);
     // if(loadMap === 'jill'){
     //   googleMap = <GoogleMapReact bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY}} defaultCenter={{ lat: 32.5059041, lng: -92.11102670000001 }} defaultZoom={12} yesIWantToUseGoogleMapApiInternals onGoogleApiLoaded={({ map, maps }) => this.addMarkers(map, maps)}></GoogleMapReact>
     // }else if(loadMap === 'john'){
@@ -65,7 +80,14 @@ class Map extends React.Component {
      <div>
        <button onClick={this.handleClick}>click me</button>
        <div style={{ height: '400px', width: '400px' }}>
-        <GoogleMapReact bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY}} defaultCenter={{ lat: 32.5059041, lng: -92.11102670000001 }} defaultZoom={12} yesIWantToUseGoogleMapApiInternals onGoogleApiLoaded={({ map, maps }) => this.addMarkers(map, maps)}></GoogleMapReact>
+        <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY}}
+        defaultCenter={{ lat: 32.5059041, lng: -92.11102670000001 }}
+        defaultZoom={12}
+        yesIWantToUseGoogleMapApiInternals
+        onGoogleApiLoaded={({ map, maps }) => this.addToState(map, maps)}
+        >
+        </GoogleMapReact>
        </div>
      </div>
    );
